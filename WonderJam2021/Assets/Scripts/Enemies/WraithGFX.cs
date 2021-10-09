2b04c00b01cee4911e13b6ea0644e2ca628f5864
CavaniNicolas <cavani.nicolas6@gmail.com>
2021-10-09 18:40:11 -0400

    [dev] implement wraith AI

---
+++ WonderJam2021/Assets/Scripts/Enemies/WraithGFX.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class WraithGFX : MonoBehaviour
+{
+
+    public AIPath m_aiPath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y, transform.localScale.z);
+        } else if (m_aiPath.desiredVelocity.x <= -0.01)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+    }
+}
