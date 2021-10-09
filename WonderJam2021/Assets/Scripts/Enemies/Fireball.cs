3ceb7c741529abc2a7fd23721b587665a607c694
CavaniNicolas <cavani.nicolas6@gmail.com>
2021-10-08 23:04:18 -0400

    [dev] improve fireball so that it destroys itself

--- WonderJam2021/Assets/Scripts/Enemies/Fireball.cs
+++ WonderJam2021/Assets/Scripts/Enemies/Fireball.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
+    // linked to the rigid body of the fireball
+    public Rigidbody2D rb;
     public float speed = 5f;
+
     private Vector2 direction; // left or right
-    private float velocity;
 
     // Start is called before the first frame update
     void Start()
     {
         direction = Vector2.right;
-        velocity = speed;
+        rb.velocity = transform.right * direction.x * speed;
+        Destroy(gameObject, 3);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-    private void FixedUpdate()
-    {
-        transform.position = transform.position += new Vector3(velocity * direction.x * Time.deltaTime, 0f, 0f);
+        Debug.Log(collision.name);
+        Destroy(gameObject);
     }
 }
