563a51ea21ee0eee1ccb024e7c6e6dd862fcf130
Harckyl <lucien.morcrette@gmail.com>
2021-10-10 03:35:12 -0400

    audio merge

--- WonderJam2021/Assets/Scripts/Items/IntroFromNPC.cs
+++ WonderJam2021/Assets/Scripts/Items/IntroFromNPC.cs
@@ -9,11 +9,12 @@ public class IntroFromNPC : MonoBehaviour
     public GameObject NPCText;
 
     public bool IntroDone = false;
-
+    private GameObject audioManager;
 
     // Start is called before the first frame update
     void Start()
     {
+        audioManager = GameObject.Find("AudioManager");
         if(IntroDone)
         {
             SetItemAvailable();
@@ -37,20 +38,28 @@ public class IntroFromNPC : MonoBehaviour
     IEnumerator HandleDialogue()
     {
         float timeToWait = 3.0f;
+        audioManager.GetComponent<AudioManager>().PlayVoice1();
         NPCText.GetComponent<DisplayTextNPC>().DisplayExactTextOnBox("Hello traveler...");
         yield return new WaitForSeconds(timeToWait);
+        audioManager.GetComponent<AudioManager>().PlayVoice2();
         NPCText.GetComponent<DisplayTextNPC>().DisplayExactTextOnBox("I see you've been struck by bad fortune");
         yield return new WaitForSeconds(timeToWait);
+        audioManager.GetComponent<AudioManager>().PlayVoice3();
         NPCText.GetComponent<DisplayTextNPC>().DisplayExactTextOnBox("This Ghost is now linked to you forever");
         yield return new WaitForSeconds(timeToWait);
+        audioManager.GetComponent<AudioManager>().PlayVoice2();
         NPCText.GetComponent<DisplayTextNPC>().DisplayExactTextOnBox("Get the artifact hidden in the castle to cut the link !");
         yield return new WaitForSeconds(timeToWait);
+        audioManager.GetComponent<AudioManager>().PlayVoice1();
         NPCText.GetComponent<DisplayTextNPC>().DisplayExactTextOnBox("Thanks to the link you can control the Ghost");
         yield return new WaitForSeconds(timeToWait);
+        audioManager.GetComponent<AudioManager>().PlayVoice3();
         NPCText.GetComponent<DisplayTextNPC>().DisplayExactTextOnBox("It will help you attack enemies !");
         yield return new WaitForSeconds(timeToWait);
+        audioManager.GetComponent<AudioManager>().PlayVoice1();
         NPCText.GetComponent<DisplayTextNPC>().DisplayExactTextOnBox("You can buy some things in my shop as well ;)");
         yield return new WaitForSeconds(timeToWait);
+        audioManager.GetComponent<AudioManager>().PlayVoice1();
         NPCText.GetComponent<DisplayTextNPC>().DisplayExactTextOnBox("Good luck !");
         yield return new WaitForSeconds(timeToWait);
 
