e173ee741c5e851241a0ead53fde33eab4ebbe49
Harckyl <lucien.morcrette@gmail.com>
2021-10-10 02:29:07 -0400

    audiomanager

---
+++ WonderJam2021/Assets/AudioManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource[] audioSource;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void PlayHeroHit()
+    {
+        //1
+        audioSource[1].Play();
+    }
+    public void PlayThunder()
+    {
+        //5
+        audioSource[5].Play();
+    }
+    public void PlayEnnemyHit()
+    {
+        //3
+        audioSource[3].Play();
+    }
+    public void PlayVoice1()
+    {
+        //0
+        audioSource[0].Play();
+    }
+    public void PlayVoice2()
+    {
+        //8
+        audioSource[8].Play();
+    }
+    public void PlayVoice3()
+    {
+        //9
+        audioSource[9].Play();
+    }
+    public void PlayPotionSound()
+    {
+        //6
+        audioSource[6].Play();
+    }
+    public void PlayPhantomSound()
+    {
+        //2
+        audioSource[2].Play();
+    }
+    public void PlayFireballSound()
+    {
+        //7
+        audioSource[7].Play();
+    }
+    public void PlayDeathSound()
+    {
+        //4
+        audioSource[4].Play();
+    }
+}
